d25badd44b7a23ceb8f3df744b3dce75b134f734
prishanandedkar <prishanandedkar@gmail.com>
2025-07-02 03:19:03 +0000

    Prisha's program

---
+++ prisha.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 12;
+    float b = 12.89;
+
+    float productOfNumbers = ( a * b);
+
+    
+
+    printf("productOfNumbers = %f \n", productOfNumbers);
+    cout << "productOfNumbers = " << productOfNumbers << endl;
+
+
+
+    return 0;
+}
