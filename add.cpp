770dd4535e0200b910e4cd9cf9d7c30749b4f9e6
prishanandedkar <prishanandedkar@gmail.com>
2025-07-02 02:24:09 +0000

    my addition test

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double pi = 3.14;
+    double radius = 2;
+
+    double areaOfCircle = pi * (radius * radius * radius);
+
+    cout << "The area of the circle is correct =" << areaOfCircle << endl;
+
+    return 0;
+}
